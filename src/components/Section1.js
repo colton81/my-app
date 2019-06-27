65ccf275b43ac5e30e996a0f119e54a9c285e733
colton8195 <coltonhillebrand92@icloud.com>
2019-06-27 12:00:43 -0700

    changed section

--- src/components/Section1.js
+++ src/components/Section1.js
@@ -1,85 +1,63 @@
-
-import React, { Component } from 'react';
-
-
-
+import React, { Component } from "react";
+import { Player, ControlBar } from "video-react";
+import VideoCover from "react-video-cover";
 
 export default class Section1 extends Component {
-    render() {
-        return (
+  render() {
+    return (
+      <header
+        style={{
+          position: "relative",
+          background_color: "black",
+          height: "75vh",
+          min_height: "25rem",
+          width: "100%",
+          background: "black",
+          overflow: "hidden"
+        }}
+      >
+        <div
+          class="overlay"
+          style={{
+            position: "absolute",
+            top: "0",
+            left: "0",
+            height: "100%",
+            width: "100%",
+            z_index: "1",
+            background: "black",
+            opacity: "0.5"
+          }}
+        >
+          <video
+            style={{
+              position: "absolute",
 
-<section
-                        className="elementor-element elementor-element-2b6102eb elementor-section-full_width elementor-section-height-default elementor-section-height-default elementor-section elementor-top-section"
-                        data-id="2b6102eb"
-                        data-element_type="section"
-                      >
-                        <div
-                          className="elementor-container elementor-column-gap-default"
-                        >
-                          <div className="elementor-row">
-                            <div
-                              className="elementor-element elementor-element-700790e8 elementor-column elementor-col-100 elementor-top-column"
-                              data-id="700790e8"
-                              data-element_type="column"
-                            >
-                              <div
-                                className="elementor-column-wrap  elementor-element-populated"
-                              >
-                                <div className="elementor-widget-wrap">
-                                  <div
-                                    className="elementor-element elementor-element-11adf46b animated-fast elementor-invisible elementor-widget elementor-widget-frel-hero-header-modern"
-                                    data-id="11adf46b"
-                                    data-element_type="widget"
-                                    data-settings='{"_animation":"pulse","_animation_delay":100}'
-                                    data-widget_type="frel-hero-header-modern.default"
-                                  >
-                                    <div className="elementor-widget-container">
-                                      <div className="cons_w_wrapper">
-                                        <div
-                                          className="fn_cs_hero_header_modern"
-                                          data-height="custom"
-                                        >
-                                          <div className="container">
-                                            <div className="content_holder">
-                                              <div className="title_holder">
-                                                <h3>
-                                                  Houston Process and Design
-                                                </h3>
-                                                <p>
-                                                  Serving the Oil, Gas, Power,
-                                                  Refining and Petrochemical
-                                                  Markets.
-                                                </p>
-                                              </div>
-                                              <div
-                                                className="btn_holder fn_cs_lightgallery"
-                                              >
-                                                <span
-                                                  className="video lightbox"
-                                                  data-src=""
-                                                  ><span className="icon"></span
-                                                  ><span className="text"></span
-                                                ></span>
-                                              </div>
-                                            </div>
-                                          </div>
-                                          <div className="bg_holder">
-                                            <div
-                                              className="o_img"
-                                              data-bg-img="wp-content/uploads/2019/06/Comp-4_Moment.jpg"
-                                            ></div>
-                                            <div className="o_color"></div>
-                                          </div>
-                                        </div>
-                                      </div>
-                                    </div>
-                                  </div>
-                                </div>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                      </section>
-                      )
-                      }
-                      }
+              min_width: "100%",
+              min_height: "100%",
+              width: "auto",
+              height: "auto",
+              z_index: "0"
+            }}
+            playsinline="playsinline"
+            autoplay="autoplay"
+            muted="muted"
+            loop="loop"
+          >
+            <source
+              src="https://storage.googleapis.com/coverr-main/mp4/Mt_Baker.mp4"
+              type="video/mp4"
+            />
+          </video>
+        </div>
+        <div
+          class="container h-100"
+          style={{ position: "relative", z_index: "2" }}
+        >
+          <h1 class="display-3">Video Header</h1>
+          <p class="lead mb-0">With HTML5 Video and Bootstrap 4</p>
+        </div>
+      </header>
+    );
+  }
+}
